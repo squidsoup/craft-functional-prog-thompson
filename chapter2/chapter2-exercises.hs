75fc44522affe47fac059955f9eef8ed04f31360
Kit Randel <kit@nocturne.net.nz>
2013-10-22 10:30:02 +1300

    added script resources for chapters 1-20

---
+++ chapter2/chapter2-exercises.hs
@@ -0,0 +1,10 @@
+-- Exercises from Chapter 2
+
+-- 2.1
+-- Define a module UsePictures which imports Pictures and
+-- contains definitions of blackHorse, rotate and rotateHorse
+-- which can use the definitions imported from Pictures
+
+module UsePictures where
+import Pictures
+
